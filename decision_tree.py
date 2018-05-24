Heavily cleaned up code for average and perceptron method, this consequently broke the SVM and Decision tree methods which will have to be fixed later.

decision_tree.py
@@ -20,38 +20,51 @@ def run_cross_validation(esbl_pos_patient_data, esbl_neg_patient_data, cross_val
         if iteration%10 is 0:
             print(str(iteration) + " ...")
 
-        while(True):
-            train_data = []
-            pos_test_data = []
-            neg_test_data = []
+        train_data, pos_test_data, neg_test_data, train_labels = process_data.generate_training_data(esbl_pos_patient_data,
+                                                                                                     esbl_neg_patient_data,
+                                                                                                     break_amount=2)
+        clf = tree.DecisionTreeClassifier()
+        clf.fit(train_data, train_labels)
 
-            train_data, pos_test_data = process_data.percentage_split([], [], esbl_pos_patient_data)
-            # testing and training data needs to exist
-            if not pos_test_data or not train_data: continue
+        pos_pred_test = clf.predict(pos_test_data)
+        neg_pred_test = clf.predict(neg_test_data)
 
-            # Data exists so append labels for all positive results
-            train_labels = ["POS" for i in range(len(train_data))]
-            train_data, neg_test_data = process_data.percentage_split(train_data, [], esbl_neg_patient_data, break_limit=len(train_labels), random_sampling=True)            
-            # control testing data needs to exist
-            if not neg_test_data: continue
+        pos_predictor_result.append(len([1 for x in pos_pred_test if x == "POS"])/float(len(pos_pred_test)))
+        neg_predictor_result.append(len([1 for x in neg_pred_test if x == "NEG"])/float(len(neg_pred_test)))
 
-            # Data exists so append leftover labels
-            for i in range(len(train_data)-len(train_labels)):
-                train_labels.append("NEG")
+    return pos_predictor_result, neg_predictor_result
 
-            # True if data can be used for analysis
-            if (train_labels):
-                break
+def run_single_patient(esbl_pos_patient_data, esbl_neg_patient_data, iterations, patient_list):
+    predictor_result = []
+
+    for iteration in range(iterations):
+
+        if iteration%10 is 0:
+            print(str(iteration) + " ...")
+
+        train_data, pos_test_data, neg_test_data, train_labels = process_data.generate_training_data(esbl_pos_patient_data,
+                                                                                                     esbl_neg_patient_data,
+                                                                                                     break_amount=1,
+                                                                                                     split_percentage=0)
+
 
         clf = tree.DecisionTreeClassifier()
         clf.fit(train_data, train_labels)
-        pos_pred_test = clf.predict(pos_test_data)
-        neg_pred_test = clf.predict(neg_test_data)
-        pos_predictor_result.append(len([1 for x in pos_pred_test if x == "POS"])/float(len(pos_pred_test)))
-        neg_predictor_result.append(len([1 for x in neg_pred_test if x == "NEG"])/float(len(neg_pred_test)))
-    return pos_predictor_result, neg_predictor_result
 
-def run(filename, CULTURE_SIZE_CUTOFF, AB_CULTURE_COUNT_CUTOFF, ESBL_AB_RESISTENCE_LIST, cross_validation=200):
+        pred_test_probabilities = clf.predict_proba(patient_list)
+
+        iteration_result = []
+        for row in pred_test_probabilities:
+            if row[0] > row[1]:
+                iteration_result.append(["NEG", row[0]])
+            else:
+                iteration_result.append(["POS", row[1]])
+
+        predictor_result.append(iteration_result)
+    
+    return predictor_result
+
+def run(filename, CULTURE_SIZE_CUTOFF, AB_CULTURE_COUNT_CUTOFF, ESBL_AB_RESISTENCE_LIST, cross_validation=100, testmode="cross_validation"):
     csv_data = data_reader.read_csv(filename)
     
     print("Converting data ...")
@@ -59,21 +72,37 @@ def run(filename, CULTURE_SIZE_CUTOFF, AB_CULTURE_COUNT_CUTOFF, ESBL_AB_RESISTEN
     ab_dict = csv_data[1]
     ab_names = ab_dict.keys()
 
-    esbl_pos_patient_data, esbl_neg_patient_data = process_data.generate_esbl_patient_data(id_dict, ab_dict, CULTURE_SIZE_CUTOFF, ESBL_AB_RESISTENCE_LIST)
-    RELATIVE_AB_CULTURE_COUNT_CUTOFF = float(len(esbl_pos_patient_data))*(float(AB_CULTURE_COUNT_CUTOFF)/100)
+    esbl_pos_patient_data, esbl_neg_patient_data = process_data.generate_data(patient_data=id_dict,
+                                                                                           ab_data=ab_dict,
+                                                                                           ab_names=ab_names,
+                                                                                           AB_CULTURE_COUNT_CUTOFF=AB_CULTURE_COUNT_CUTOFF,
+                                                                                           CULTURE_SIZE_CUTOFF=CULTURE_SIZE_CUTOFF,
+                                                                                           ESBL_AB_RESISTENCE_LIST=ESBL_AB_RESISTENCE_LIST,
+                                                                                           esbl_result_format=None,
+                                                                                           numeric=True)
 
-    esbl_pos_ab_count = process_data.count_ab(esbl_pos_patient_data, ab_names)
-    esbl_neg_ab_count = process_data.count_ab(esbl_neg_patient_data, ab_names)
-    relevant_ab_list = process_data.relevant_ab(esbl_pos_ab_count, ab_dict, ab_names, RELATIVE_AB_CULTURE_COUNT_CUTOFF)
+    if testmode == "cross_validation":
+        print("Running cross validation " + str(cross_validation) + " times ...")
+        pos_predictor_result, neg_predictor_result = run_cross_validation(esbl_pos_patient_data, esbl_neg_patient_data, cross_validation)
 
-    esbl_pos_patient_data = process_data.filter_ab(esbl_pos_patient_data, relevant_ab_list, ab_dict, None, numeric=True)
-    esbl_neg_patient_data = process_data.filter_ab(esbl_neg_patient_data, relevant_ab_list, ab_dict, None, numeric=True)
+        print("RESULTS OF TRAINING DECISION TREE:")
+        print("Average accuracy of Esbl pos: " + str(np.mean(pos_predictor_result)))
+        print("Standard deviation of Esbl pos: " + str(np.std(pos_predictor_result)))
+        print("Average accuracy of Esbl neg: " + str(np.mean(neg_predictor_result)))
+        print("Standard deviation of Esbl neg: " + str(np.std(neg_predictor_result)))
 
-    print("Running cross validation " + str(cross_validation) + " times ...")
-    pos_predictor_result, neg_predictor_result = run_cross_validation(esbl_pos_patient_data, esbl_neg_patient_data, cross_validation)
+    elif testmode == "test_patient":
+        patient_list = [esbl_neg_patient_data[3767]]
+        temp = []
+        for train_patient in esbl_pos_patient_data:
+            for test_patient in patient_list:
+                if test_patient != train_patient:
+                    temp.append(train_patient)
 
-    print("RESULTS OF TRAINING DECISION TREE:")
-    print("Average accuracy of Esbl pos: " + str(np.mean(pos_predictor_result)))
-    print("Standard deviation of Esbl pos: " + str(np.std(pos_predictor_result)))
-    print("Average accuracy of Esbl neg: " + str(np.mean(neg_predictor_result)))
-    print("Standard deviation of Esbl neg: " + str(np.std(neg_predictor_result)))
+        if len(temp) < len(esbl_pos_patient_data):
+            print("Removed {} patient(s) from training data ...".format(len(esbl_pos_patient_data) - len(temp)))
+
+        print("Running on test set of {} patient(s) ...".format(len(patient_list)))
+        predictor_result = run_single_patient(temp, esbl_neg_patient_data, cross_validation, patient_list)
+        print("RESULTS OF TRAINING DECISION TREE:")
+        print(predictor_result)

perceptron.py
@@ -1,78 +1,110 @@
 # Libraries
 import numpy as np
-import pandas as pd
 from datetime import datetime
-
 from sklearn.neural_network import MLPClassifier
-import matplotlib.pyplot as plt
-import matplotlib.font_manager
+
 # Modules
+import data
 import data_reader
 import process_data
-import data_reader
 
-def run_cross_validation(esbl_pos_patient_data, esbl_neg_patient_data, cross_validation):
-    pos_predictor_result = []
-    neg_predictor_result = []
-    for iteration in range(cross_validation):
+class Perceptron():
+    """docstring for Perceptron"""
+    def __init__(self, filename,
+                       CULTURE_SIZE_CUTOFF=0,
+                       AB_CULTURE_COUNT_CUTOFF=0,
+                       ESBL_AB_RESISTENCE_LIST = ["cefotaxim", "ceftazidim", "ceftriaxon"],
+                       cross_validation=100,
+                       testmode="cross_validation"):
+        self.data = data.Representation(filename)
+        self.data.set_culture_parameters(CULTURE_SIZE_CUTOFF=CULTURE_SIZE_CUTOFF,
+                                         AB_CULTURE_COUNT_CUTOFF=AB_CULTURE_COUNT_CUTOFF,
+                                         ESBL_AB_RESISTENCE_LIST=ESBL_AB_RESISTENCE_LIST)
+        self.cross_validation = cross_validation
+        self.testmode = testmode
+        self.patient_list = []
 
-        if iteration%10 is 0:
-            print(str(iteration) + " ...")
+    def run_cross_validation(self):
+        pos_predictor_result = []
+        neg_predictor_result = []
+        for iteration in range(self.cross_validation):
 
-        while(True):
-            train_data = []
-            pos_test_data = []
-            neg_test_data = []
+            if iteration%10 is 0:
+                print(str(iteration) + " ...")
 
-            train_data, pos_test_data = process_data.percentage_split([], [], esbl_pos_patient_data)
-            # testing and training data needs to exist
-            if not pos_test_data or not train_data: continue
+            train_data, pos_test_data, neg_test_data, train_labels = process_data.generate_training_data(self.data,
+                                                                                                         break_amount=1.2,
+                                                                                                         split_percentage=20)
 
-            # Data exists so append labels for all positive results
-            train_labels = ["POS" for i in range(len(train_data))]
-            train_data, neg_test_data = process_data.percentage_split(train_data, [], esbl_neg_patient_data, break_limit=len(train_labels), random_sampling=True)            
-            # control testing data needs to exist
-            if not neg_test_data: continue
+            clf = MLPClassifier(solver='lbfgs', alpha=1e-3, hidden_layer_sizes=(len(self.data.ab_names), 2), random_state=1)
+            clf.fit(train_data, train_labels)
 
-            # Data exists so append leftover labels
-            for i in range(len(train_data)-len(train_labels)):
-                train_labels.append("NEG")
+            pos_pred_test = clf.predict(pos_test_data)
+            neg_pred_test = clf.predict(neg_test_data)
 
-            # True if data can be used for analysis
-            if (train_labels):
-                break
+            pos_predictor_result.append(len([1 for x in pos_pred_test if x == "POS"])/float(len(pos_pred_test)))
+            neg_predictor_result.append(len([1 for x in neg_pred_test if x == "NEG"])/float(len(neg_pred_test)))
 
-        clf = MLPClassifier(solver='lbfgs', alpha=1e-3, hidden_layer_sizes=(32, 5), random_state=2)
-        clf.fit(train_data, train_labels)
-        pos_pred_test = clf.predict(pos_test_data)
-        neg_pred_test = clf.predict(neg_test_data)
-        pos_predictor_result.append(len([1 for x in pos_pred_test if x == "POS"])/float(len(pos_pred_test)))
-        neg_predictor_result.append(len([1 for x in neg_pred_test if x == "NEG"])/float(len(neg_pred_test)))
-    return pos_predictor_result, neg_predictor_result
+        return pos_predictor_result, neg_predictor_result
 
-def run(filename, CULTURE_SIZE_CUTOFF, AB_CULTURE_COUNT_CUTOFF, ESBL_AB_RESISTENCE_LIST, cross_validation=50):
-    csv_data = data_reader.read_csv(filename)
-    
-    print("Converting data ...")
-    id_dict = csv_data[0]
-    ab_dict = csv_data[1]
-    ab_names = ab_dict.keys()
+    def run_single_patient(self):
+        predictor_result = []
 
-    esbl_pos_patient_data, esbl_neg_patient_data = process_data.generate_esbl_patient_data(id_dict, ab_dict, CULTURE_SIZE_CUTOFF, ESBL_AB_RESISTENCE_LIST)
-    RELATIVE_AB_CULTURE_COUNT_CUTOFF = float(len(esbl_pos_patient_data))*(float(AB_CULTURE_COUNT_CUTOFF)/100)
+        for iteration in range(self.cross_validation):
 
-    esbl_pos_ab_count = process_data.count_ab(esbl_pos_patient_data, ab_names)
-    esbl_neg_ab_count = process_data.count_ab(esbl_neg_patient_data, ab_names)
-    relevant_ab_list = process_data.relevant_ab(esbl_pos_ab_count, ab_dict, ab_names, RELATIVE_AB_CULTURE_COUNT_CUTOFF)
+            if iteration%10 is 0:
+                print(str(iteration) + " ...")
 
-    esbl_pos_patient_data = process_data.filter_ab(esbl_pos_patient_data, relevant_ab_list, ab_dict, None, numeric=True)
-    esbl_neg_patient_data = process_data.filter_ab(esbl_neg_patient_data, relevant_ab_list, ab_dict, None, numeric=True)
+            train_data, pos_test_data, neg_test_data, train_labels = process_data.generate_training_data(self.data,
+                                                                                                         break_amount=1,
+                                                                                                         split_percentage=0)
 
-    print("Running cross validation " + str(cross_validation) + " times ...")
-    pos_predictor_result, neg_predictor_result = run_cross_validation(esbl_pos_patient_data, esbl_neg_patient_data, cross_validation)
+            clf = MLPClassifier(solver='lbfgs', alpha=1e-3, hidden_layer_sizes=(15, 5), random_state=1)
+            clf.fit(train_data, train_labels)
 
-    print("RESULTS OF MULTILAYER PERCEPTRON TREE:")
-    print("Average accuracy of Esbl pos: " + str(np.average(pos_predictor_result)))
-    print("Standard deviation of Esbl pos: " + str(np.std(pos_predictor_result)))
-    print("Average accuracy of Esbl neg: " + str(np.average(neg_predictor_result)))
-    print("Standard deviation of Esbl neg: " + str(np.std(neg_predictor_result)))
+            pred_test_probabilities = clf.predict_proba(self.patient_list)
+
+            iteration_result = []
+            for row in pred_test_probabilities:
+                if row[0] > row[1]:
+                    iteration_result.append(["NEG", row[0]])
+                else:
+                    iteration_result.append(["POS", row[1]])
+
+            predictor_result.append(iteration_result)
+        
+        return predictor_result
+
+
+    def run(self):        
+        print("Converting data ...")
+
+        self.data.load_filtered_esbl_patient_data()
+
+        if self.testmode == "cross_validation":
+            print("Running cross validation " + str(self.cross_validation) + " times ...")
+            pos_predictor_result, neg_predictor_result = self.run_cross_validation()
+            
+            print("RESULTS OF MULTILAYER PERCEPTRON:")
+            print("Average accuracy of Esbl pos: " + str(np.average(pos_predictor_result)))
+            print("Standard deviation of Esbl pos: " + str(np.std(pos_predictor_result)))
+            print("Average accuracy of Esbl neg: " + str(np.average(neg_predictor_result)))
+            print("Standard deviation of Esbl neg: " + str(np.std(neg_predictor_result)))
+        
+        elif self.testmode == "test_patient":
+            self.patient_list = [self.data.esbl_pos_patient_data[0]]
+
+            temp = []
+            for train_patient in self.data.esbl_pos_patient_data:
+                for test_patient in self.patient_list:
+                    if test_patient != train_patient:
+                        temp.append(train_patient)
+
+            if len(temp) < len(self.data.esbl_pos_patient_data):
+                self.data.esbl_pos_patient_data = temp
+                print("Removed {} patient(s) from training data ...".format(len(self.data.esbl_pos_patient_data) - len(temp)))
+
+            print("Running on test set of {} patient(s) ...".format(len(self.patient_list)))
+            predictor_result = self.run_single_patient()
+            print("RESULTS OF MULTILAYER PERCEPTRON:")
+            print(predictor_result)

process_data.py
@@ -1,6 +1,11 @@
 # Libraries
 import numpy as np
+import itertools as it
 from random import randint
+from math import floor
+
+# Modules
+import data
 
 def count_ab(esbl_patient_data, ab_names):
     ab_count = np.zeros((len(ab_names), 2))
@@ -45,20 +50,20 @@ def filter_ab(patient_data, relevant_ab_list, ab_dict, esbl_result, numeric):
         esbl_patient_data.append(patient_ab)
     return esbl_patient_data
 
-def percentage_split(train_data, test_data, esbl_patient_data, split_percentage=20, break_limit=-1, random_sampling=False):
+def percentage_split(train_data, test_data, esbl_patient_data, split_percentage, sample_count=-1, random_sampling=False):
     break_count = 0
-    if break_limit > 0 and random_sampling:
+    if sample_count > 0 and random_sampling:
         data_length = len(esbl_patient_data)-1
-        for i in range(break_limit):
-            if randint(0, 100) <= split_percentage:
+        for i in range(sample_count):
+            if randint(0, 100) < split_percentage:
                 test_data.append(esbl_patient_data[randint(0, data_length)])
             else:
                 train_data.append(esbl_patient_data[randint(0, data_length)])
     else:
         for data_row in esbl_patient_data:
-            if break_count == break_limit: break
+            if break_count == sample_count: break
             break_count+=1
-            if randint(0, 100) <= split_percentage:
+            if randint(0, 100) < split_percentage:
                 test_data.append(data_row)
             else:
                 train_data.append(data_row)
@@ -72,6 +77,7 @@ def find_esbl_pos_day(patient_data, ESBL_AB_RESISTENCE_LIST):
         if (ab in ESBL_AB_RESISTENCE_LIST and result is not 'S' and "M_banaal_BK" in bepaling):
             return culture[0]
     return None
+
 
 def generate_esbl_patient_data(id_dict, ab_dict, CULTURE_SIZE_CUTOFF, ESBL_AB_RESISTENCE_LIST):
     esbl_pos_patient_data = []
@@ -96,7 +102,6 @@ def generate_esbl_patient_data(id_dict, ab_dict, CULTURE_SIZE_CUTOFF, ESBL_AB_RE
             culture_day = culture[0]
             ab = culture[1]
             result = culture[2]
-
             if culture_day == esbl_pos_day:
                 esbl_found = True
                 # Remove this break if you want to include the cultures of the day
@@ -108,7 +113,7 @@ def generate_esbl_patient_data(id_dict, ab_dict, CULTURE_SIZE_CUTOFF, ESBL_AB_RE
             skip = False
             if (previous_culture_day is not None and esbl_pos_day is not None):
                 total_days= abs((previous_culture_day-esbl_pos_day).days)
-                if (total_days < 10):
+                if (total_days < 2 or total_days > 360):
                     pass
                     skip = True
 
@@ -129,3 +134,64 @@ def generate_esbl_patient_data(id_dict, ab_dict, CULTURE_SIZE_CUTOFF, ESBL_AB_RE
     print("ESBL Positive patients: " + str(len(esbl_pos_patient_data)))
     print("ESBL Negative patients: " + str(len(esbl_neg_patient_data)))
     return esbl_pos_patient_data, esbl_neg_patient_data
+
+def generate_data(patient_data, ab_data, ab_names, AB_CULTURE_COUNT_CUTOFF, CULTURE_SIZE_CUTOFF, ESBL_AB_RESISTENCE_LIST, esbl_result_format=None, numeric=True):
+    
+    esbl_pos_patient_data, esbl_neg_patient_data = generate_esbl_patient_data(patient_data, ab_data, CULTURE_SIZE_CUTOFF, ESBL_AB_RESISTENCE_LIST)
+
+    RELATIVE_AB_CULTURE_COUNT_CUTOFF = float(len(esbl_pos_patient_data))*(float(AB_CULTURE_COUNT_CUTOFF)/100)
+
+    esbl_pos_ab_count = count_ab(esbl_pos_patient_data, ab_names)
+    esbl_neg_ab_count = count_ab(esbl_neg_patient_data, ab_names)
+    relevant_ab_list = relevant_ab(esbl_pos_ab_count, ab_data, ab_names, RELATIVE_AB_CULTURE_COUNT_CUTOFF)
+
+    esbl_pos_patient_data = filter_ab(esbl_pos_patient_data, relevant_ab_list, ab_data, esbl_result=esbl_result_format, numeric=numeric)
+    esbl_neg_patient_data = filter_ab(esbl_neg_patient_data, relevant_ab_list, ab_data, esbl_result=esbl_result_format, numeric=numeric)
+
+    return esbl_pos_patient_data, esbl_neg_patient_data
+
+def generate_training_data(data_representation, break_amount=1, split_percentage=20):
+    while(True):
+        train_data = []
+        pos_test_data = []
+        neg_test_data = []
+
+        if break_amount == 1:
+            train_data, pos_test_data = percentage_split([], [],
+                                                         data_representation.esbl_pos_patient_data,
+                                                         sample_count=int(break_amount*len(data_representation.esbl_pos_patient_data)),
+                                                         split_percentage=split_percentage)
+        else:
+            train_data_temp, pos_test_data = percentage_split([], [],
+                                                              data_representation.esbl_pos_patient_data,
+                                                              sample_count=int(break_amount*len(data_representation.esbl_pos_patient_data)),
+                                                              random_sampling=True, split_percentage=split_percentage)
+            train_data = []
+            for row in train_data_temp:
+                if row not in pos_test_data:
+                    train_data.append(row)
+            pos_test_data.sort()
+            pos_test_data = list(k for k,_ in it.groupby(pos_test_data))
+
+        # testing and training data needs to exist
+        if ((not pos_test_data or not train_data) and (split_percentage > 0 and split_percentage < 100)): continue
+
+        # Data exists so append labels for all positive results
+        train_labels = ["POS" for i in range(len(train_data))]
+        train_data, neg_test_data = percentage_split(train_data, [], data_representation.esbl_neg_patient_data, sample_count=len(train_labels), random_sampling=True, split_percentage=split_percentage)            
+ 
+        # control testing data needs to exist
+        if (not neg_test_data and (split_percentage > 0 and split_percentage < 100)): continue
+
+        # Data exists so append leftover labels
+        for i in range(len(train_data)-len(train_labels)):
+            train_labels.append("NEG")
+
+
+        # True if data can be used for analysis
+        if (train_labels):
+            break
+
+    return train_data, pos_test_data, neg_test_data, train_labels
+
+    